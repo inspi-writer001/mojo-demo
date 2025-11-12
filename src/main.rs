30d1b6885b19b43da5661e7ab0036ee9e2d97aed
inspiration_gx <conservedinnovation@gmail.com>
2025-11-12 13:21:06 +0100

    fix: made resources modular

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,14 @@ fn main() {
 }
 
 #[derive(Resource)]
-struct PlayerKeypair(Keypair);
-
-#[derive(Resource)]
-struct PlayerStateName(String);
-
-#[derive(Resource, Clone, Copy)]
-struct PlayerPosition(MyPosition);
-
-#[derive(Resource)]
-struct ErClient(WorldClient);
-
-#[derive(Resource)]
-struct LastMoveTimer(Timer);
+struct GameState {
+    keypair: Keypair,
+    state_name: String,
+    position: MyPosition,
+    rpc_client: WorldClient,
+    er_client: WorldClient,
+    last_move_timer: Timer,
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let payer = read_keypair_file("dev_wallet-keypair.json").expect("Couldn't find wallet file");
@@ -43,8 +38,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let rpc_client = WorldClient::new("https://api.devnet.solana.com");
     let er_rpc_client = WorldClient::new("https://devnet-eu.magicblock.app/");
 
-    let new_world = World::create_world(&rpc_client, &payer, "moving_game6");
-    let state_name = "brother_position6";
+    let new_world = World::create_world(&rpc_client, &payer, "moving_game7");
+    let state_name = "brother_position7";
     println!("we got world yayy, {}", new_world.unwrap());
 
     let player_position = MyPosition { x: 0.0, y: 0.0 };
@@ -56,11 +51,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     );
 
     // Insert resources so they can be accessed in systems
-    commands.insert_resource(PlayerKeypair(payer));
-    commands.insert_resource(PlayerStateName(state_name.to_string()));
-    commands.insert_resource(PlayerPosition(player_position));
-    commands.insert_resource(ErClient(er_rpc_client));
-    commands.insert_resource(LastMoveTimer(Timer::from_seconds(3.0, TimerMode::Once)));
+    commands.insert_resource(GameState {
+        keypair: payer,
+        state_name: state_name.to_string(),
+        position: player_position,
+        rpc_client,
+        er_client: er_rpc_client,
+        last_move_timer: Timer::from_seconds(2.0, TimerMode::Once),
+    });
 
     commands.spawn(Camera2dBundle {
         camera_2d: Camera2d {
@@ -85,49 +83,50 @@ fn character_movement(
     mut characters: Query<(&mut Transform, &Sprite)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    keypair: Res<PlayerKeypair>,
-    state_name: Res<PlayerStateName>,
-    mut player_position: ResMut<PlayerPosition>,
-    mut last_move_timer: ResMut<LastMoveTimer>,
-    rpc_client: Res<ErClient>,
+    mut game_state: ResMut<GameState>,
 ) {
     let mut has_moved = false;
+
     for (mut transform, _) in &mut characters {
-        // Update local state IMMEDIATELY for smooth gameplay
         if input.pressed(KeyCode::Up) {
             transform.translation.y += 100.0 * time.delta_seconds();
-            player_position.0.y += 100.0 * time.delta_seconds();
+            game_state.position.y += 100.0 * time.delta_seconds();
             has_moved = true;
         }
         if input.pressed(KeyCode::Down) {
             transform.translation.y -= 100.0 * time.delta_seconds();
-            player_position.0.y -= 100.0 * time.delta_seconds();
+            game_state.position.y -= 100.0 * time.delta_seconds();
             has_moved = true;
         }
         if input.pressed(KeyCode::Right) {
             transform.translation.x += 100.0 * time.delta_seconds();
-            player_position.0.x += 100.0 * time.delta_seconds();
+            game_state.position.x += 100.0 * time.delta_seconds();
             has_moved = true;
         }
         if input.pressed(KeyCode::Left) {
             transform.translation.x -= 100.0 * time.delta_seconds();
-            player_position.0.x -= 100.0 * time.delta_seconds();
+            game_state.position.x -= 100.0 * time.delta_seconds();
             has_moved = true;
         }
     }
 
-    // If player moved, reset the timer
+    // Reset timer if moved
     if has_moved {
-        last_move_timer.0.reset();
+        game_state.last_move_timer.reset();
     }
 
-    // Tick the timer (only counts up when player is NOT moving)
-    last_move_timer.0.tick(time.delta());
+    // Tick the timer
+    game_state.last_move_timer.tick(time.delta());
 
-    // After 3 seconds of NO movement, send to MagicBlock
-    if last_move_timer.0.just_finished() {
-        match World::write_state(&rpc_client.0, &keypair.0, &state_name.0, &player_position.0) {
-            Ok(hash) => println!("Synced position to ER after inactivity: {}", hash),
+    // Send to MagicBlock after 2 seconds of inactivity
+    if game_state.last_move_timer.just_finished() {
+        match World::write_state(
+            &game_state.er_client,
+            &game_state.keypair,
+            &game_state.state_name,
+            &game_state.position,
+        ) {
+            Ok(hash) => println!("Synced position to ER: {}", hash),
             Err(e) => eprintln!("Failed to sync to ER: {}", e),
         }
     }
